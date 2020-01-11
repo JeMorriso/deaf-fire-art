e187662267830d40d4b0a674af83682221a327a4
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-01-11 13:27:46 -0800

    Add multer and remove express-busboy

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const ejs = require('ejs');
+const multer = require('multer');
 
 // get express application
 const app = express();
