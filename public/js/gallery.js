57a2c5adc8018307081da4afd1f977a91b4c6bb0
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-01-30 11:21:24 -0800

    Add basic email popup form

---
+++ public/js/gallery.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("email-popup").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("email-popup").style.display = "none";
+}
