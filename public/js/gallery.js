f807e8eb9f7e376ceffe8918bb0e57243972cdb7
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-03-03 23:54:45 -0800

    Implement front-end logic for deleting images, and cancelling delete

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -25,6 +25,28 @@ for(let i = 0; i < imageIcons.length; i++) {
   })
 }
 
+// only exists if admin logged in
+var deleteIcons = document.getElementsByClassName("delete-image");
+
+for (let i = 0; i < deleteIcons.length; i++) {
+  deleteIcons[i].addEventListener("click", () => {
+    if (window.getComputedStyle(document.getElementById("delete-button")).getPropertyValue("display") === "none") {
+      document.getElementById("delete-button").style.display = "inline-block";
+    }
+    if (window.getComputedStyle(document.getElementById("cancel-button")).getPropertyValue("display") === "none") {
+      document.getElementById("cancel-button").style.display = "inline-block";
+    }
+    deleteIcons[i].parentElement.style.display = "none";
+  })
+}
+
+document.getElementById("cancel-button").addEventListener("click", () => {
+  var galleryImages = document.getElementsByClassName("gallery-image");
+  for (let i = 0; i < galleryImages.length; i++) {
+    galleryImages[i].style.display = "block";    
+  }
+})
+
 // function openFormWithDescription(itemDescription) {
 //   document.getElementById("email-popup").style.display = "block";
 //   document.getElementById("email-subject").value = itemDescription;
