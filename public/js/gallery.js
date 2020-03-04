9b4d812b5aaf0b9f53c0e57d13d8afa22967cce2
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-03-03 22:35:33 -0800

    Refactor button CSS. Add logic for email icon to show and hide email form, while also automatically setting email subject to be the item's description

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,7 +1,32 @@
-function openForm() {
+// this function wasn't working as a callback
+// function openForm() {
+//   document.getElementById("email-popup").style.display = "block";
+// }
+
+document.getElementById("open-button").addEventListener("click", () => {
   document.getElementById("email-popup").style.display = "block";
+});
+
+document.getElementById("cancel-button").addEventListener("click", () => {
+  document.getElementById("email-popup").style.display = "none";
+  // clear text
+  document.getElementById("email-address").value = "";
+  document.getElementById("email-subject").value = "";
+  document.getElementById("email-body").value = "";
+});
+
+var imageIcons = document.getElementsByClassName("email-icon");
+
+for(let i = 0; i < imageIcons.length; i++) {
+  imageIcons[i].addEventListener("click", function() {
+    var itemDescription = imageIcons[i].parentElement.previousElementSibling.innerText;
+    document.getElementById("email-popup").style.display = "block";
+    document.getElementById("email-subject").value = itemDescription;
+  })
 }
 
-function closeForm() {
-  document.getElementById("email-popup").style.display = "none";
-}
+// function openFormWithDescription(itemDescription) {
+//   document.getElementById("email-popup").style.display = "block";
+//   document.getElementById("email-subject").value = itemDescription;
+// }
+// }
