1e1b33632d5446599916d830a79e87775f92be0a
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-05-13 16:11:11 -0700

    Cleanup commit

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -43,11 +43,8 @@ router.get('/gallery', isLoggedIn, (req, res) => {
     if (err) {
       return console.log(err);
     }
-    console.log(results)
     var gallery_images = [];
     results.forEach((row) => {
-      console.log(row.file_prefix);
-      console.log(row.item_description)
       gallery_images.push({image_url_prefix: process.env.BUCKET_URL + row.file_prefix, item_description: row.item_description, item_price: row.item_price, file_prefix: row.file_prefix});
     });
     // // first parameter is the ejs file to be rendered - 2nd one is data being passed in (RHS) and what it's being named (LHS)
@@ -96,7 +93,6 @@ router.post('/gallery', isLoggedIn, (req, res, next) => {
                   if (err) {
                     console.log(err);
                   } else {
-                    console.log("the key: " + data.Key)
                     // data.Key must be equal to small_fname
                     const item_description = req.body['item-description'];
                     const item_price = req.body['item-price'];
@@ -158,7 +154,6 @@ router.post('/gallery/delete', (req, res) => {
         }
       }); 
   });
-  res.send({hello: 'gubna'}); 
 });
 
 router.post('/email', (req, res) => {
