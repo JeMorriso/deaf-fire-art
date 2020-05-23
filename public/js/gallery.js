684998e7effe5e27813433aebb482945cde8f5c0
Jeremy Morrison <jeremy.morrison36@gmail.com>
2020-05-22 19:19:43 -0700

    Add check for is user logged in

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,87 +1,96 @@
-document.getElementById("open-button").addEventListener("click", () => {
-  document.getElementById("email-popup").style.display = "block";
+// check if the user is logged in
+isLoggedIn = false;
+if (document.getElementsByClassName('admin').length > 0) {
+  isLoggedIn = true;
+}
+
+// Email form event listeners
+document.getElementById('open-button').addEventListener('click', () => {
+  document.getElementById('email-popup').style.display = 'block';
 });
 
-document.getElementById("cancel-button").addEventListener("click", () => {
-  document.getElementById("email-popup").style.display = "none";
+document.getElementById('cancel-button').addEventListener('click', () => {
+  document.getElementById('email-popup').style.display = 'none';
   // clear text
-  document.getElementById("email-address").value = "";
-  document.getElementById("email-subject").value = "";
-  document.getElementById("email-body").value = "";
+  document.getElementById('email-address').value = '';
+  document.getElementById('email-subject').value = '';
+  document.getElementById('email-body').value = '';
 });
 
-var imageIcons = document.getElementsByClassName("email-icon");
-var toggle = "block";
+// Email image icon event listeners
+var imageIcons = document.getElementsByClassName('email-icon');
+var toggle = 'block';
 
 for (let i = 0; i < imageIcons.length; i++) {
-  imageIcons[i].addEventListener("click", function () {
+  imageIcons[i].addEventListener('click', function () {
     var itemDescription =
       imageIcons[i].parentElement.previousElementSibling.innerText;
-    document.getElementById("email-popup").style.display = toggle;
-    toggle = toggle === "block" ? "none" : "block";
-    document.getElementById("email-subject").value = itemDescription;
+    document.getElementById('email-popup').style.display = toggle;
+    toggle = toggle === 'block' ? 'none' : 'block';
+    document.getElementById('email-subject').value = itemDescription;
   });
 }
 
-// only exists if admin logged in
-// TODO: try catch
-var deleteIcons = document.getElementsByClassName("delete-image");
+// Admin event listeners
+if (isLoggedIn) {
+  var deleteIcons = document.getElementsByClassName('delete-image');
 
-for (let i = 0; i < deleteIcons.length; i++) {
-  deleteIcons[i].addEventListener("click", () => {
-    if (
-      window
-        .getComputedStyle(document.getElementById("delete-button"))
-        .getPropertyValue("display") === "none"
-    ) {
-      document.getElementById("delete-button").style.display = "inline-block";
-    }
-    if (
-      window
-        .getComputedStyle(document.getElementById("cancel-delete"))
-        .getPropertyValue("display") === "none"
-    ) {
-      document.getElementById("cancel-delete").style.display = "inline-block";
-    }
-    deleteIcons[i].parentElement.parentElement.style.display = "none";
-  });
-}
-
-document.getElementById("cancel-delete").addEventListener("click", () => {
-  var galleryImages = document.getElementsByClassName("gallery-image");
-  for (let i = 0; i < galleryImages.length; i++) {
-    galleryImages[i].style.display = "block";
-  }
-});
-
-document.getElementById("delete-button").addEventListener("click", () => {
-  // stores all the files that are to be deleted on the backend
-  const data = [];
-
-  var galleryImages = document.getElementsByClassName("gallery-image");
-  for (let i = 0; i < galleryImages.length; i++) {
-    // if display is set to none, add file name to be deleted on backend
-    if (galleryImages[i].style.display === "none") {
-      data.push(galleryImages[i].dataset.filename);
-      console.log(data);
-    }
-  }
-
-  fetch("/gallery/delete", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log("Success:", data);
-      //alert("Images successfully deleted!")
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+  for (let i = 0; i < deleteIcons.length; i++) {
+    deleteIcons[i].addEventListener('click', () => {
+      if (
+        window
+          .getComputedStyle(document.getElementById('delete-button'))
+          .getPropertyValue('display') === 'none'
+      ) {
+        document.getElementById('delete-button').style.display = 'inline-block';
+      }
+      if (
+        window
+          .getComputedStyle(document.getElementById('cancel-delete'))
+          .getPropertyValue('display') === 'none'
+      ) {
+        document.getElementById('cancel-delete').style.display = 'inline-block';
+      }
+      deleteIcons[i].parentElement.parentElement.style.display = 'none';
     });
-});
+  }
+
+  document.getElementById('cancel-delete').addEventListener('click', () => {
+    var galleryImages = document.getElementsByClassName('gallery-image');
+    for (let i = 0; i < galleryImages.length; i++) {
+      galleryImages[i].style.display = 'block';
+    }
+  });
+
+  document.getElementById('delete-button').addEventListener('click', () => {
+    // stores all the files that are to be deleted on the backend
+    const data = [];
+
+    var galleryImages = document.getElementsByClassName('gallery-image');
+    for (let i = 0; i < galleryImages.length; i++) {
+      // if display is set to none, add file name to be deleted on backend
+      if (galleryImages[i].style.display === 'none') {
+        data.push(galleryImages[i].dataset.filename);
+        console.log(data);
+      }
+    }
+
+    fetch('/gallery/delete', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log('Success:', data);
+        //alert("Images successfully deleted!")
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  });
+}
